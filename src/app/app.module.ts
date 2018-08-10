f6d9a618dd484090270c41a6663aeec04ef07501
Francisco Mendoza <francisco.mendoza@envato.com>
2018-08-10 15:03:39 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { WorkbenchComponent } from './components/workbench/workbench.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { EditorComponent } from './components/cvs/editor/editor.component';
+import { PreviewComponent } from './components/cvs/preview/preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkbenchComponent,
+    DashboardComponent,
+    StatsComponent,
+    EditorComponent,
+    PreviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/cvs/editor/editor.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-editor',
+    templateUrl: './editor.component.html',
+    styleUrls: ['./editor.component.css']
+})
+export class EditorComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
